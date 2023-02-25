102ed5f4b711447b4c5615eb32a108e5dc9f38ae
Shahil Patel <shahilp@gmail.com>
2023-02-25 18:01:15 -0500

    API working

--- APIcall/test.py
+++ APIcall/test.py
@@ -7,19 +7,29 @@ from requests.auth import HTTPBasicAuth
 #auth = HTTPBasicAuth('x-api-key', 'fd03d916c4ff4d1885e54662d18c7f32')
 spoonacular_api_key = 'fd03d916c4ff4d1885e54662d18c7f32'
 
-url = 'https://api.spoonacular.com/recipes/findByNutrients'
+url = 'https://api.spoonacular.com/recipes/findByIngredients?'
 
-min_protein = 10
+ingredientList = ["apples","flour","sugar","plums"]
 
-query_params = "apiKey=" + spoonacular_api_key + \
- "&min_protein=" + str(min_protein)
+postString = "&ingredients="
+for count,val in enumerate(ingredientList):
+    if count == 0:
+        postString = postString + val
+    else:
+        postString = postString+"+"+val
+    
+    postString = postString+","
+postString = postString[:len(postString)-1]
+amountOfRecipes = 2
+postString = postString+"&number=" + str(amountOfRecipes)
+query_params = "apiKey=" + spoonacular_api_key + postString
+#"&ingredients=apples,+flour,+sugar&number=2&ignorePantry=False"
 
-query = url + "?" + query_params
+query = url  + query_params
 
 spoonacular_response = requests.get(query)
 
 print(spoonacular_response.json())
-#print.pprint(spoonacular_response.json())
 
 
 
